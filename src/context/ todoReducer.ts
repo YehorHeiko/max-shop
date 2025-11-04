81ef82667601e207fc13903c5536d2cd1358f4b9
Yehor Heiko <yehorheiko.developer@gmail.com>
2025-11-05 00:14:40 +0100

    fix: add console logs for debugging ADD_TODO and DELETE_TODO actions

--- src/context/ todoReducer.ts
+++ src/context/ todoReducer.ts
@@ -1,8 +1,10 @@
 export const TodoReducer = (state, action) => {
   switch (action.type) {
     case "ADD_TODO":
+        console.log(state)
         return {...state, todo: [...state.todo, {id: Math.random(), text: action.payload}] }
     case "DELETE_TODO":
+        console.log(state)
         return {...state,  todo: state.todo.filter((i) => i.id !== action.payload)}
     default:
         return state
